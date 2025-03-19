b3dd809e51c0db05659a503b0894e42a3981e3fc
Kent Anthony Engbino <kentanthony070495@gmail.com>
2025-03-19 17:05:41 +0800

    March 19, 2025

---
+++ src/views/OCR-textview.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <div class="d-flex align-items-center">
+      <v-icon @click="goBack" class="mt-2 me-3" size="x-large" icon="mdi-chevron-double-left" title="Back"></v-icon>
+      <h1>Extracted OCR Content</h1>
+    </div>
+    <p class="text-teal-darken-3">{{ extractedText }}</p>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'OCR',
+  computed: {
+    extractedText() {
+      return this.$route.query.text || "No content available.";
+    },
+  },
+  methods: {
+    goBack() {
+      this.$router.go(-1);
+    },
+  },
+};
+</script>
