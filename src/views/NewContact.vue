1c5835d06f5f4d25581609420ebb7a10cd6ac298
Kent Anthony Engbino <kentanthony070495@gmail.com>
2025-05-07 12:46:21 +0800

    May 07, 2025

--- src/views/NewContact.vue
+++ src/views/NewContact.vue
@@ -728,7 +728,7 @@ export default {
             this.first_name = clientData.first_name || '';
             this.middle_name = clientData.middle_name || '';
             this.last_name = clientData.last_name || '';
-            this.suffix = clientData.suffix || '';
+            this.suffix = this.getTitle(clientData.suffix, this.suffixesItems, 'suffix') || '';
             this.initial = clientData.initial || '';
             this.display_name = clientData.display_name || '';
             this.gender = clientData.gender || '';
